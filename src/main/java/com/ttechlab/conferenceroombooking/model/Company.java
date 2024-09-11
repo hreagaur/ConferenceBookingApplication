ca5b787bafe62d842d4b64a588d50bc53fc1d2be
PrajanUShettyy <prajanshetty45@gmail.com>
2024-09-11 11:15:45 +0530

    Company Attributes Created

--- src/main/java/com/ttechlab/conferenceroombooking/model/Company.java
+++ src/main/java/com/ttechlab/conferenceroombooking/model/Company.java
@@ -1,4 +1,52 @@
 package com.ttechlab.conferenceroombooking.model;
 
-public class Company {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="Company")
+public class Company 
+{
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="companyId")
+	private Long companyId;
+	@Column(name="companyName")
+	private String companyName;
+	@Column(name="email",unique=true)
+	private String email;
+	@Column(name="phoneNumber")
+	private Long phoneNumber;
+	public Long getCompanyId() {
+		return companyId;
+	}
+	public void setCompanyId(Long companyId) {
+		this.companyId = companyId;
+	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Long getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(Long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	
 }
